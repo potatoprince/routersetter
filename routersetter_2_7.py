8e335f31f9d3ff867d679f63d2485d026cd052f8
potatoprince <martins.berzins16@gmail.com>
2018-06-07 14:12:23 +0300

    Added Config for CCR1036-12G-4S and traffic generator

--- routersetter_2_7.py
+++ routersetter_2_7.py
@@ -26,10 +26,21 @@ bridges_RB3011 =("/interface bridge\nadd name=e1e2\nadd name=e3e4\nadd name=e6e7
 add bridge=e3e4 interface=ether3\nadd bridge=e3e4 interface=ether4\nadd bridge=e6e7 interface=ether6\n\
 add bridge=e6e7 interface=ether7\nadd bridge=e8e10 interface=ether8\nadd bridge=e8e10 interface=ether10\n\n\n")
 
+bridges_CCR1036_12G_4S =("/interface bridge\nadd name=e1e2\nadd name=e3e4\nadd name=e5e6\n\
+add name=e7e8\nadd name=e9e10\nadd name=e11e12\nadd name=s1s2\nadd name=s3s4\n\n\
+/interface bridge port\nadd bridge=e1e2 interface=ether1\nadd bridge=e1e2 interface=ether2\n\
+add bridge=e3e4 interface=ether3\nadd bridge=e3e4 interface=ether4\nadd bridge=e5e6 interface=ether5\n\
+add bridge=e5e6 interface=ether6\nadd bridge=e7e8 interface=ether7\nadd bridge=e7e8 interface=ether8\n\
+add bridge=e9e10 interface=ether9\nadd bridge=e9e10 interface=ether10\nadd bridge=e11e12 interface=ether11\n\
+add bridge=e11e12 interface=ether12\nadd bridge=s1s2 interface=sfp1\nadd bridge=s1s2 interface=sfp2\n\
+add bridge=s3s4 interface=sfp3\nadd bridge=s3s4 interface=sfp3\nadd bridge=s3s4 interface=sfp4\n\n\n")
+
 n = 0 # Vlan counter
 v = 1 # Number of board config cycles
 ipaCount = 0 #ethertnet IP address count
 sfpCount = 0 # sfp address count
+macCount = 0 # mac address count for traffic generator
+
 routerNameORetherCount = raw_input("Use rb name or ethernet count? ( input rb or eth ): ")
 
 f = open("fconfig","w")
@@ -37,15 +48,21 @@ f.write(alwaysWrite)
 
 # Code for getting pre-made config
 if routerNameORetherCount == "rb":
-    routerboardName = raw_input('Routerboard name ')
+    infoPrompt = raw_input("Would you like to see the list of available router configurations and numbers?: ")
+    if (infoPrompt =="yes" or "y"):
+        print("There are router configurations for :\n\
+        1) RB3011\n\
+        2) CCR1036-12G-4S\n")
+    routerboardName = raw_input('Input Routerboard number: ')
+
     
-    if routerboardName =="RB3011":
+    if routerboardName == '1':
         f.write(bridges_RB3011)
         boardCount = input("How many boards to test: ")
         stendIP = raw_input("What is the stend IP: ")
         while (boardCount >= v and boardCount <=5):
             ipEnd = stendIP[len(stendIP)-2:len(stendIP)]
-            f.write("#Board "+str(v)+"\n\n")
+            f.write("                                 #Board "+str(v)+"\n\n")
             sfpCount+=1
             ipaCount+=1
             f.write("/ip address\nadd address=172.2"+str(ipaCount)+"."+ipEnd+".21/26 interface=ether5\n")
@@ -65,9 +82,68 @@ add interface=ether9 name=vlan"+str(n)+"1 vlan-id="+str(n)+"1\n")
 add address=192.168."+str(n)+"1.10/24 interface=vlan"+str(n)+"1\n\n\n")
             v+=1
 
+    if routerboardName == '2':
+        f.write(bridges_CCR1036_12G_4S)
+        boardCount = input("how many boards to test: ")
+        stendIP = raw_input("What is the stend IP: ")
+        if len(stendIP) == 13:
+            ipEnd = stendIP[len(stendIP)-2:len(stendIP)]
+        elif len(stendIP) == 14:
+            ipEnd = stendIP[len(stendIP)-3:len(stendIP)]   
+        stendEtherStart = input("Which stend ether is connected to board 1 ether1?: ")
+        stendEtherEnd = stendEtherStart
+        stendSfpStart =input("Which stend sfp is connected to board 1 sfp1?: ")
+        stendSfpEnd = stendSfpStart
+        while(boardCount >=v and boardCount <= 2):
+            f.write("#Board "+str(v)+"\n\n")
+            f.write("/ip address\nadd address=172.2"+str(stendEtherEnd)+"."+ipEnd+".21/26 interface=ether1\n")
+            stendEtherEnd+=1
+            f.write("add address=172.2"+str(stendEtherEnd)+"."+ipEnd+".21/26 interface=ether12\n")
+            stendEtherEnd+=1
+            f.write("add address=172.2"+str(stendSfpEnd)+"."+ipEnd+".121/26 interface=sfp1\n")
+            stendSfpEnd+=1
+            f.write("add address=172.2"+str(stendSfpEnd)+"."+ipEnd+".121/26 interface=sfp4\n\n\n")
+            stendSfpEnd+=1
+            v+=1
+
+        trafficGenerator = raw_input("Make traffic generator?: ")
+        if(trafficGenerator == "yes"):
+            while(stendEtherStart < stendEtherEnd ):
+                f.write("/tool traffic-generator packet-template\nadd interface=ether"+str(stendEtherStart)+" \
+ip-dst=172.2"+str(stendEtherStart+1)+"."+str(ipEnd)+".21 \
+mac-dst="+raw_input("Put in the mac-dst for ether"+str(stendEtherStart))+" name=e"+str(stendEtherStart)+"e"+str(stendEtherStart+1)+" \
+random-ranges=29:8:1-21,34:16:60000-60001,36:16:50000-50001\n")
+                stendEtherStart+=1
+                f.write("add interface=ether"+str(stendEtherStart)+" ip-dst 172.2"+str(stendEtherStart-1)+"."+str(ipEnd)+".21 \
+mac-dst="+raw_input("put in the mac-dst for ether"+str(stendEtherStart))+" name=e"+str(stendEtherStart)+"e"+str(stendEtherStart-1)+" \
+random-ranges=29:8:1-21,34:16:60000-60001,36:16:50000-50001\n")
+                stendEtherStart+=1
+
+            while(stendSfpStart < stendSfpEnd):
+                f.write("add interface=sfp"+str(stendSfpStart)+" ip-dst=172.2"+str(stendSfpStart+1)+"."+str(ipEnd)+".102 \
+mac-dst="+raw_input("put in the mac-dst for sfp"+str(stendSfpStart))+" name=sfp"+str(stendEtherStart)+"sfp"+str(stendSfpStart+1)+" \
+random-ranges=29:8:1-21,34:16:60000-60001,36:16:50000-50001\n")
+                stendSfpStart+=1
+                f.write("add interface=sfp"+str(stendSfpStart)+" ip-dst=172.2"+str(stendSfpStart-1)+"."+str(ipEnd)+".102 \
+mac-dst="+raw_input("put in the mac-dst for sfp"+str(stendSfpStart))+ "name=sfp"+str(stendSfpStart)+"sfp"+str(stendSfpStart-1)+" \
+random-ranges=29:8:1-21,34:16:60000-60001,36:16:50000-50001\n\n\n")
+                stendSfpStart+=1
+
+                f.write("           #Higher CPU Load\n/lcd set backlight-timeout=never\n/ip firewall connection tracking set enabled=yes\n\
+/interface bridge settings set use-ip-firewall=yes\n\n\n")
+        f.write("           #Remove old logs and start new logging\n\
+/file remove log.0.txt\n/file remove log.1.txt\n/tool graphing interface add store-on-disk=no;\n\
+/tool graphing resource add store-on-disk=no;\n/system reboot\n\n\n")
+        f.write("           #Set fresh logs\n/system logging set [find topics~\"error\"] action=disk\n\
+/system logging set [find topics=\"warning\"] action=disk\n/system logging set [find topics=\"critical\"] action=disk\n\
+/system logging action set memory memory-lines=1;\n:delay 1s;\n/system logging action set memory memory-lines=1000;\n\
+/interface ethernet reset-counters [find];\n/log info message=\"------- LOGS AND ETHERNET COUNTERS CLEARED -------\"\n\
+/log print\n\n\n")
+        
 
 
-# Code for making "unique" config
+
+# Code for making "unique" config #ToDo
 elif routerNameORetherCount == "eth":
     etherCount = input("How many ethernet bridges?: ")
     while etherCount > 0:
@@ -99,12 +175,3 @@ elif routerNameORetherCount == "eth":
 
 
 f.close()
-
-#file.write("bridge")
-
-# with open(routerboardName, "r") as f:
-#     searchlines = f.readlines()
-# for i, line in enumerate(searchlines):
-#     if "bridge" in line: 
-#         for l in searchlines[i:i+5]: print l,
-#         print
